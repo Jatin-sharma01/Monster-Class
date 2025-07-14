94805b41042335cdb3a82f6f8efc74c82ef651ca
Jatin-sharma01 <amobsidian001@gmail.com>
2025-07-15 00:46:12 +0530

    updated

--- react-demos/src/App.jsx
+++ react-demos/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Practise1 from "./components/state-hooks/Practise1";
 import Practise2 from "./components/state-hooks/Practise2";
+import Practise3 from "./components/state-hooks/Practise3";
 
 function App() {
   return (
     <div>
       {/* <Practise1 /> */}
-      <Practise2 />
+      {/* <Practise2 /> */}
+      <Practise3 />
     </div>
   );
 }
